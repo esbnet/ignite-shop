5a988515da61fbc22f97976a4dac2a3cef119465
Edmilson Soares <esbnet@gmail.com>
2022-11-25 01:50:27 -0300

    style: :lipstick: create style product

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
-import type { AppProps } from 'next/app'
-import { globalStyles } from '../../styles/globals'
+import type { AppProps } from "next/app"
+import { globalStyles } from "../../styles/globals"
+
+import Image from "next/image"
+
+import { Container, Header } from "../../styles/pages/app"
+
+import logoImg from "../assets/logo.svg"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} width={130} height={52} alt=''/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,52 @@
+import Image from "next/image"
+
 import { styled } from "../../styles"
+import { HomeContainer, Product } from "../../styles/pages/home"
+
+import camisa1 from "../assets/shirt1.png"
+import camisa2 from "../assets/shirt2.png"
+import camisa3 from "../assets/shirt3.png"
+import camisa4 from "../assets/shirt4.png"
 
 const Button = styled("button", {
   background: "$gree300",
   borderRadius: 4,
   border: 0,
-  padding: '4px 8px',
+  padding: "4px 8px",
 
   span: {
     fontWeight: "bold",
   },
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
 })
 
 export default function Home() {
   return (
-    <>
-      <Button>
-        <span>Teste </span>
-        Enviar
-        </Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camisa1} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camisa2} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camisa3} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
