625cff1294828aa7b0d9184850e787c7b2b4a043
Edmilson Soares <esbnet@gmail.com>
2022-11-28 10:15:04 -0300

    feat: :sparkles: add slide at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image"
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import { styled } from "../../styles"
 import { HomeContainer, Product } from "../../styles/pages/home"
+
+import 'keen-slider/keen-slider.min.css'
 
 import camisa1 from "../assets/shirt1.png"
 import camisa2 from "../assets/shirt2.png"
 import camisa3 from "../assets/shirt3.png"
-import camisa4 from "../assets/shirt4.png"
 
 const Button = styled("button", {
   background: "$gree300",
@@ -24,23 +27,36 @@ const Button = styled("button", {
 })
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa2} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa3} width={520} height={480} alt='' />
         <footer>
           <strong>Camiseta X</strong>
